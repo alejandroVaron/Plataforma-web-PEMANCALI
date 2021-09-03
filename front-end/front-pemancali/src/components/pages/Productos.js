50588e3172969f8f54520ca6f7fe413328dad0da
cesar <cesar.solano@correounivalle.edu.co>
2021-09-03 18:45:06 -0500

    footer y card pagina productos

--- front-end/front-pemancali/src/components/pages/Productos.js
+++ front-end/front-pemancali/src/components/pages/Productos.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Footer from '../footer/Footer'
+import Cards from '../cards/Cards'
 
 const Productos = () => {
     return (
         <div>
-            <h1>Nuestos Productos</h1>
+        <>  
+            <Cards/>
+            <Footer/>
+                    
+        </>
         </div>
+        
     )
+    
 }
 
 export default Productos
