bed8562113dcef1b6f0528bba1faa2855cbbd9ec
alejandroVaron <alejandro.varon@correounivalle.edu.co>
2021-09-12 13:32:49 -0500

    category and user End-point

--- back-end/src/routes/userRoutes.js
+++ back-end/src/routes/userRoutes.js
@@ -7,5 +7,6 @@ router.post('/', UserController.createUser)
 router.put('/id=:id', UserController.updateUser)
 router.delete('/id=:id', UserController.deleteUser)
 router.get('/id=:id', UserController.getUserById)
+router.get('/email=:email', UserController.getUserByEmail)
 
 export default router;
